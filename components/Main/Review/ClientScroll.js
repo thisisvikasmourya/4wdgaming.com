2dcb54efb53f64cd84f0b0e2dd786044477ef162
vikasmourya <60725905+thisisvikasmourya@users.noreply.github.com>
2023-08-07 16:32:06 +0530

    done

--- components/Main/Review/ClientScroll.js
+++ components/Main/Review/ClientScroll.js
@@ -11,6 +11,7 @@ import client8 from "../../../public/images/Untitled-design-93.png";
 // import client9 from "../../../public/images/Yudiz-Solutions-Limited-Logo.png";
 import client10 from "../../../public/images/wcc.png";
 import client11 from "../../../public/images/trinity.png";
+import client12 from "../../../public/images/gl.png";
 
 const ClientScroll = () => {
   return (
@@ -41,13 +42,13 @@ const ClientScroll = () => {
           {/* <li className="marquee-item">
             {" "}
             <Image
-              src={client3}
+              src={client12}
               alt="client1"
               className="rounded-lg"
               width={200}
               height={100}
             />
-          &nbsp;&nbsp;&nbsp;&nbsp;
+          
           </li> */}
           &nbsp;&nbsp;&nbsp;&nbsp;
           <li className="marquee-item">
@@ -193,6 +194,16 @@ const ClientScroll = () => {
             {" "}
             <Image
               src={client8}
+              alt="client1"
+              className="rounded-lg bg-white"
+              width={160}
+              height={90}
+            />
+          </li>
+          <li className="marquee-item">
+            {" "}
+            <Image
+              src={client12}
               alt="client1"
               className="rounded-lg bg-white"
               width={160}
